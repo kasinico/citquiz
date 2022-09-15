dc5be717e5fb6875fed74bf14670465b5c26704a
kasinico <kasiryenicholas@gmail.com>
2022-09-15 16:05:45 +0300

    updating labels on play button and color text

--- number7.py
+++ number7.py
@@ -1,13 +1,1 @@
-#number 7
-# Write a function called show_stars(rows). If rows is 5, it should print the following:
-
-
-rows = 5
-for i in range(0, rows):
-    #print(1)
-    #loop for each column
-    for j in range(0, i + 1):
-        # 
-        print("*", end=' ')
-    #each row
-    print("\r")
+print(20/17)
