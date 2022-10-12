e5fa1f9e4892233569d96858bb20f650be43e194
kasinico <kasiryenicholas@gmail.com>
2022-10-12 17:33:04 +0300

    add web job posting

---
+++ web_scrapping/web.py
@@ -0,0 +1,28 @@
+
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+from bs4 import BeautifulSoup
+import os
+
+
+html_path = os.path.join(os.path.dirname(__file__), 'web.html')
+url = "https://cit-projects-2021.github.io/fake-jobs/"
+# print(html_path)
+
+with open(html_path, 'r') as html_file:
+    doc = html_file.read()
+
+soup = BeautifulSoup(doc, 'html.parser')
+# print(soup.prettify())
+title = soup.title.text
+anchors = soup.find_all('a')
+# print(anchors)
+# italics = soup.find('i')
+# _italics = soup.find_all('b')[-1].find('i').text.strip()
+b_tags = soup.find_all('b')
+last_item = b_tags[-1]
+child_item = last_item.find('i')
+child_text = child_item.text.strip()
